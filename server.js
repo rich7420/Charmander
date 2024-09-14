bea28e0db27642b9d7cee355a7f6ca87d4913f03
rich7420 <rc910420@gmail.com>
2024-09-15 01:36:49 +0800

    try10

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 // 使用 PORT 環境變數，或默認為 8080
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(port, () => {
+    console.log('Hello world listening on port', port);
+  });  
